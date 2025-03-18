9e6bf9419d3d4f2c1e9d3f47a9c110d00dc8ad20
Drynkin Sergey <sergsmile29@gmail.com>
2025-03-18 20:30:36 +0200

    update vercel deploy

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
       </div>
-      <p>Hello</p>
+
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   );
