cb93469331620220f90b6e180fd0c1296fa28283
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-03-26 19:36:48 +0200

    useState

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,20 +2,35 @@ import { useState } from "react";
 import s from "./Counter.module.css";
 
 export const Counter = () => {
+  const [counter, setCounter] = useState(33);
+  const [step, setStep] = useState(1);
+
   const HendlePlusClick = (e) => {
-    console.log(e);
+    setCounter(counter + step);
   };
-  const sayHello = (name) => {
-    console.log(`Hello, ${name}`);
+  const HendleMinusClick = (e) => {
+    setCounter(counter - step);
   };
+  const HendleResetClick = () => {
+    setCounter(0);
+    setStep(1);
+  };
+
   return (
     <div className={s.flexContainer}>
       <div className={s.wrapper}>
-        <h1>{1}</h1>
+        <h1>{counter}</h1>
+        <input value={step} onChange={(e) => setStep(+e.target.value)}></input>
         <div className={s.flex}>
-          <button className="btn">minus</button>
-          <button onClick={() => sayHello("Igor")}> reset</button>
-          <button onClick={HendlePlusClick}>plus</button>
+          <button onClick={HendleMinusClick} className="btn">
+            minus
+          </button>
+          <button onClick={HendleResetClick} className="btn">
+            reset
+          </button>
+          <button onClick={HendlePlusClick} className="btn">
+            plus
+          </button>
         </div>
       </div>
     </div>
